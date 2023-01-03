cac8d08615459edcf2ed8e34b854b3917fce439f
cerennnnn <cerengunnes@gmail.com>
2023-01-03 23:12:29 +0300

    1st hw is completed.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+let city: String = "New York"
+let country: String = "USA"
+let phoneNumber: String = "02111233212"
+let postCode: String = "12345"
+let email: String = "example@gmail.com"
+let job: String = " developer"
+var stockAmount: Int = 2
+let customerName: String = "Josh"
+var balance: Double = 10.0
+let birthday: String = "08.08.1997"
+var salary: Int = 5_000
+var maritalStatus: String = "Single"
+var productReview: String = "The app is really good!"
+
+let paymentDate: String = "04.03.21"
+var payment: String = "Credit Card"
+var orderAmount: Int = 3
+let carModel: String = "Mercedes"
+let bookName: String = "Atomic Habits"
+let releaseDate: String = "10.16.2018"
+let discountAmount: Int = 25
+let roomNumber: Int = 5
+let latitude: Double = 41.40338
+let longitude: Double = 2.17403
+let productName: String = "Chips"
+let foodPrice: Double = 10.50
+
+let brand: String = "Apple"
+let musicName: String = "Anti-hero by Taylor Swift"
+let videoDuration: Double = 3.21
+var productRating: Int = 5
+let pictureName: String = "car_picture"
+let fileFormat: String = ".pdf"
+let color: String = "Gray"
+let colorCode: String = "#808080"
+let phoneModel: String = "iPhone SE"
+let screenSize: Double = 4.7
+let weight: Double = 5.09
+
+let nationalDay: String = "October 29th - Republic Day"
+let holiday: String = "Christmas"
+let reservationDate: String = "01.01.2019"
+let streetName: String = "Broadway"
+let busLine: String = "174"
+var minutesRemaining: Int = 6
+let trackingNumber: String = "123451234512"
+var couponTime: Int = 20
+let couponCode: String = "zxcv12"
+let billAdress: String = "1234 Main Street"
